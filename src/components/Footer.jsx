2dc03f3eb8aedb257ccb88bb69e892d2639bc2c1
Dat Le <datle1108@gmail.com>
2024-09-18 12:44:10 +0200

    Update layout, add sidebar stuff

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2050. Copyright by DatLe.</small>
+      <p>
+        Version <b>1.9</b>
+      </p>
+    </footer>
+  );
+}
