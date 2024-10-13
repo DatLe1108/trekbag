9f40070f687b2531c1d403c7d3a7e613f712f099
Dat Le <datle1108@gmail.com>
2024-10-13 11:20:04 +0200

    Centralize logic for add item

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,10 +1,34 @@
+import { useRef, useState } from "react";
 import Button from "./Button";
 
-export default function AddItemForm() {
+export default function AddItemForm({ handleAddItem }) {
+  const [itemText, setItemText] = useState("");
+  const inputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!itemText) {
+      alert("Item cannot be empty");
+      inputRef.current.focus();
+      return;
+    }
+
+    handleAddItem(itemText);
+    setItemText("");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
-      <input type="text" />
+      <input
+        ref={inputRef}
+        value={itemText}
+        onChange={(e) => {
+          setItemText(e.target.value);
+        }}
+        autoFocus
+      />
       <Button>Add to list</Button>
     </form>
   );
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import BackgroundHeading from "./BackgroundHeading";
 import Footer from "./Footer";
 import Header from "./Header";
 import ItemList from "./ItemList";
 import Sidebar from "./Sidebar";
+import { initialItems } from "../lib/constants";
 
 function App() {
+  const [items, setItems] = useState(initialItems);
+
+  const handleAddItem = (newItemText) => {
+    const newItem = {
+      id: new Date().getTime(),
+      name: newItemText,
+      package: false,
+    };
+
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
+
   return (
     <>
       <BackgroundHeading />
@@ -12,9 +27,9 @@ function App() {
       <main>
         <Header />
 
-        <ItemList />
+        <ItemList items={items} />
 
-        <Sidebar />
+        <Sidebar handleAddItem={handleAddItem} />
       </main>
 
       <Footer />
